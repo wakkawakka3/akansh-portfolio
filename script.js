116d6db0d0da6999489714e1a0adab2f848e3fcd
Akansh Rawat <akanshrawat.in@gmail.com>
2025-08-05 22:38:48 +0530

    added new feature of name pronounciation added the link for my blog

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//work in progress
+function speakName() {
+    const utterance = new SpeechSynthesisUtterance("Ekansh Rawat");
+    utterance.lang = "en-US";
+    speechSynthesis.speak(utterance);
+  }
