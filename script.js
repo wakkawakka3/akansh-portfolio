5c0a58dd83a6714136a45b1be5daee05f8ef2853
Akansh Rawat <akanshrawat.in@gmail.com>
2025-08-09 21:59:14 +0530

    added local timer to the webpage and also added icon for the name sound utternace

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function speakName() {
-    const utterance = new SpeechSynthesisUtterance("Ekansh Rawat");
-    utterance.lang = "en-US";
-    speechSynthesis.speak(utterance);
-  }
+  const sound = new SpeechSynthesisUtterance("Ekansh Rawat");
+  sound.lang = "en-US";
+  speechSynthesis.speak(sound);
+}
+
+function livechat(){
+    const message = document.getElementsByClassName("message")
+}
+function updateClock() {
+  const localtime = document.getElementsByClassName("local-time")[0];
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours.toString().padStart(2, "0");
+  minutes = minutes.toString().padStart(2, "0");
+  seconds = seconds.toString().padStart(2, "0");
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  localtime.innerHTML = `
+    <span class="clock-icon">🕐</span> 
+    <span class="live-time">${timeString}</span>    
+    <span class="timezone">IST (UTC+5:30)</span>
+  `;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
